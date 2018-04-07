7dd0f2b27e9091ab05fd61b7d2a11f8df429ee31
Tedster <tedster@tedster.net>
2018-04-07 04:44:52 +0100

    decrapify slightly

--- main.cpp
+++ main.cpp
@@ -8,18 +8,12 @@ int main(int argc, char **argv)
     }
     else
     {
-        //std::string currarg;
         bool NotPrime;
-        int num;
-        int i, j;
-
+        int num, i, j;
         for(i=1; i<argc; i++)
         {
             NotPrime = false;
             num = std::stoi(argv[i]);
-            //std::istringstream ss(argv);
-            //currarg = argv[i];
-            //ss >> num;
             if(num%2==0)
             {
                 NotPrime=true;
@@ -47,9 +41,7 @@ int main(int argc, char **argv)
             {
                 std::cout << num << " appears to be prime" << std::endl;
             }
-
         }
-
     }
     return 0;
 }
